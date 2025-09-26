2c9c53e0c73b1b5acc3fc1ac3349e6547258f804
DimasAnjayMabar <gregoriusnovantino17@gmail.com>
2025-09-26 13:05:45 +0700

    upload

--- api/index.php
+++ api/index.php
@@ -1,6 +1,2 @@
 <?php
-// Check if storage link exists, if not create it
-if (!file_exists(__DIR__ . '/../public/storage')) {
-    symlink(__DIR__ . '/../storage/app/public', __DIR__ . '/../public/storage');
-}
 require __DIR__ . '/../public/index.php';
